6736024294312a87a31ca50ed728001578501d8d
Filip Palmqvist <fille.palmqvist@icloud.com>
2020-04-12 10:10:45 +0200

    Added a dummy Xcode project

---
+++ MoBlog/MoBlog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoBlog
+//
+//  Created by Filip Palmqvist on 2020-04-12.
+//  Copyright © 2020 Filip Palmqvist. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
